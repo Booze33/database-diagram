9c13b67370d8c93445e4bf876c3d832fbdb173d8
Christian Onoh <chibyk5000@gmail.com>
2023-06-14 00:12:56 +0100

    Add treatments and invoice_item tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -8,7 +8,7 @@ CREATE TABLE patients (
 CREATE TABLE medical_histories (
   id INT GENERATED ALWAYS AS IDENTITY,
   admitted_at TIMESTAMP,
-  patient_id INT REFERENCES patient(id) ON DELETE CASCADE,
+  patient_id INT REFERENCES patients(id) ON DELETE CASCADE,
   status VARCHAR(255) NOT NULL
 );
 
@@ -18,4 +18,18 @@ CREATE TABLE invoices (
   generated_at TIMESTAMP NOT NULL,
   payed_at TIMESTAMP NOT NULL,
   medical_history_id INT REFERENCES medical_histories(id)
+);
+
+CREATE TABLE treatments (
+  id INT REFERENCES medical_histories(id) ON DELETE CASCADE,
+  type VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL
+)
+
+CREATE TABLE invoice_item (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  unit_price DECIMAL NOT NULL,
+  quantity INT, total_price DECIMAL NOT NULL,
+  invoice_id INT REFERENCES invoices(id) ON DELETE CASCADE,
+  treatment_id INT REFERENCES treatments(id) ON DELETE CASCADE
 );
